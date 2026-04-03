02b23a56d8ec00456773f490296c2afc16a08e4a
Maicon Berlofa <maicon.berloffa@gmail.com>
2026-04-03 20:02:19 -0300

    feat(blog): enforce content standards in CI

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,8 @@
 import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
+import { AUTHORS } from './data/authors';
+
+const AUTHOR_IDS = Object.keys(AUTHORS) as [string, ...string[]];
 
 const blog = defineCollection({
   loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
@@ -7,7 +10,7 @@ const blog = defineCollection({
     title: z.string(),
     description: z.string(),
     date: z.coerce.date(),
-    authorId: z.string(),
+    authorId: z.enum(AUTHOR_IDS),
     tags: z.array(z.string()).default([]),
     image: z.string().optional(),
     coverImage: z.string().optional(),
