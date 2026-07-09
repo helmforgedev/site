5c7e53e7f32b5c47dcda917ee6bc6de0041b2374
Maicon Berlofa <maicon.berloffa@gmail.com>
2026-07-09 11:14:30 -0300

    docs(community): refresh contributors list

---
+++ src/data/contributors.ts
@@ -0,0 +1,52 @@
+export type Contributor = {
+  name: string;
+  login: string;
+  profileUrl: string;
+  avatarUrl: string;
+  contributions: number;
+};
+
+export const chartContributors: Contributor[] = [
+  {
+    name: 'Maicon Berlofa',
+    login: 'mberlofa',
+    profileUrl: 'https://github.com/mberlofa',
+    avatarUrl: 'https://github.com/mberlofa.png?size=96',
+    contributions: 612,
+  },
+  {
+    name: 'Mathieu REHO',
+    login: 'mreho',
+    profileUrl: 'https://github.com/mreho',
+    avatarUrl: 'https://github.com/mreho.png?size=96',
+    contributions: 12,
+  },
+  {
+    name: 'ummon-v',
+    login: 'ummon-v',
+    profileUrl: 'https://github.com/ummon-v',
+    avatarUrl: 'https://github.com/ummon-v.png?size=96',
+    contributions: 3,
+  },
+  {
+    name: 'Volodymyr',
+    login: 'volodymyr-devops',
+    profileUrl: 'https://github.com/volodymyr-devops',
+    avatarUrl: 'https://github.com/volodymyr-devops.png?size=96',
+    contributions: 2,
+  },
+  {
+    name: 'Dennis Rieks',
+    login: 'drieks',
+    profileUrl: 'https://github.com/drieks',
+    avatarUrl: 'https://github.com/drieks.png?size=96',
+    contributions: 1,
+  },
+  {
+    name: 'Radek Crlik',
+    login: 'rcrlik',
+    profileUrl: 'https://github.com/rcrlik',
+    avatarUrl: 'https://github.com/rcrlik.png?size=96',
+    contributions: 1,
+  },
+];
